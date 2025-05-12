695455ceb7acbf6f3d790e16ccaf9123c7ee2522
chocomountain1 <git-commit-address>
2025-05-12 19:13:11 -0400

    feature(flask_app): setting the parameters in order to flask can run

---
+++ tarea2/flask_app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, render_template, redirect,url_for, session
+#from utils.validations 
+from database import db
+from werkzeug.utils import secure_filename
+import filetype
+import os
+
+UPLOAD_FOLDER = 'static/uploads'
+
+app = Flask(__name__)
+
+app.secret_key = "s3cr3t_k3y"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
