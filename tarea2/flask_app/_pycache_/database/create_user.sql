2f4e815f23d72de706c7f4b103c4f2e093dc935d
chocomountain1 <git-commit-address>
2025-05-12 18:18:17 -0400

    feature(database): create the user of the database, in this case the user cc5002

---
+++ tarea2/flask_app/_pycache_/database/create_user.sql
@@ -0,0 +1,8 @@
+-- Active: 1747088022784@@localhost@3306
+-- Crear usuario
+
+CREATE USER 'cc5002'@'localhost' IDENTIFIED BY 'cc5002'
+
+--Eliminar el usuario de ser ncecesario
+
+DROP USER 'cc5002'@'localhost'
